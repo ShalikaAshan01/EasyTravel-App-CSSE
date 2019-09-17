2211e8d2df10b774570097ab6972600f3ba9619a
Pasan Godamune <pasanjg@gmail.com>
2019-09-17 07:35:15 +0530

    otp confirm page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,27 +11,55 @@ export class LoginPage implements OnInit {
   showProgressBar: boolean = false;
   showOTPinput: boolean = false;
 
-  constructor() { }
+  phoneNumber: any;
+  otp: any;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  async login() {
+  login() {
+
     this.showProgressBar = true;
     console.log("login function started", this.showProgressBar);
 
     // TODO: Write login function here.
 
     // this.showProgressBar = false;
-    // await console.log("login end", this.showProgressBar);
+    // console.log("login end", this.showProgressBar);
 
 
-    await setInterval(() => {
-      console.log("number sent");
+    setTimeout(() => {
+      console.log("number sent", this.phoneNumber);
       this.showProgressBar = false;
+      this.showOTPinput = true;
+      console.log("login end", this.showProgressBar);
 
-    },
-      3000)
+    }, 3000)
+  }
+
+  sendOTP() {
+
+    this.showProgressBar = true;
+    console.log("otp confirm function started", this.showProgressBar);
+
+    // TODO: Write OTP function here.
+
+    // this.showProgressBar = false;
+    // console.log("login end", this.showProgressBar);
+
+
+    setTimeout(() => {
+      console.log("otp confirm sent", this.otp);
+      this.showProgressBar = false;
+      console.log("otp confirm end", this.showProgressBar);
+
+      // TODO: Navigate to Home.
+      this.showOTPinput = false;
+      this.router.navigate(['/home']);
+
+    }, 3000)
   }
 
 }
