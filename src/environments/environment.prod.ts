d2cc171489a6fbade1d02698aec6108fc09698f9
Chathura Nilanka <chathuranilanka1024@gmail.com>
2019-09-30 09:05:22 +0530

    Final changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBAc48YBIK63fGIc3yFowHCFkEQ4L40SjA",
-    authDomain: "easy-travel-6013a.firebaseapp.com",
-    databaseURL: "https://easy-travel-6013a.firebaseio.com",
-    projectId: "easy-travel-6013a",
-    storageBucket: "easy-travel-6013a.appspot.com",
-    messagingSenderId: "569327319360",
-    appId: "1:569327319360:web:e6abcb7887a5cebb27e2a5"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
